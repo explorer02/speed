4f0c97cb4c6a63208083a6a81dd24516429f2429
Ajay <ajay.bhardwaj@sprinklr.com>
2022-02-26 18:59:32 +0530

    update dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 // styles
 import 'styles/globals.css';
-import 'fontsource-roboto';
+import '@fontsource/roboto';
 
 // components
 import { Layout } from 'containers/Layout';
